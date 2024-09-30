df37d94a9fb6117839218ec48edb09468c4d3134
Kanak Agrawal <kanak.avinash.agrawal@gmail.com>
2024-09-30 20:14:25 +0530

    Deployed website

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/task-gdsc/'
-});
+  base: '/task-gdsc/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+},});
+
+
